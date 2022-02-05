4bcac8c7a65d9348e24d6da37da6e106467c134c
Mathieu Soysal <Mathieu.Soysal@etu.umontpellier.fr>
2022-02-05 17:29:17 +0100

    🔨 Validation test DatedPUzzlesDao

--- src/main/java/io/github/mathieusoysal/dao/DatedPuzzlesDao.java
+++ src/main/java/io/github/mathieusoysal/dao/DatedPuzzlesDao.java
@@ -1,10 +1,14 @@
 
 package io.github.mathieusoysal.dao;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.github.mathieusoysal.codingame_stats.puzzle.Puzzle;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 
@@ -13,18 +17,34 @@ import org.bson.Document;
 import io.github.mathieusoysal.model.DatedPuzzle;
 
 public class DatedPuzzlesDao extends AbstractDao {
+        public static final DateTimeFormatter DATE_TIME_MONGODB_FORMAT = DateTimeFormatter
+                        .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'+00:00'");
+        public static final Gson gson = new GsonBuilder()
+                        .setDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSSXXX")
+                        .create();
+        public static final String PUZZLES_HISTORY_COLLECTION = "puzzles-history";
 
-    public static final String PUZZLES_HISTORY_COLLECTION = "puzzles-history";
+        private MongoCollection<Document> collection;
 
-    private MongoCollection<Document> collection;
+        public DatedPuzzlesDao(MongoClient mongoClient, String databaseName) {
+                super(mongoClient, databaseName);
+                collection = db.getCollection(PUZZLES_HISTORY_COLLECTION);
+        }
 
-    public DatedPuzzlesDao(MongoClient mongoClient, String databaseName) {
-        super(mongoClient, databaseName);
-        collection = db.getCollection(PUZZLES_HISTORY_COLLECTION);
-    }
+        public List<DatedPuzzle> getPuzzlesOf(LocalDate date) {
+                var result = new ArrayList<Document>();
+                var queryBetweenTwoDate = createQueryBetweenTwoDate(date);
+                collection.find(new Document("date", queryBetweenTwoDate)).into(result);
+                return result.parallelStream().map(Document::toJson).map(d -> gson.fromJson(d, DatedPuzzle.class)).toList();
+        }
 
-    public List<DatedPuzzle> getPuzzlesOf(Date date) {
-        return null;
-    }
+        private Document createQueryBetweenTwoDate(LocalDate givenDate) {
+                var dateFrom = LocalDateTime.of(givenDate.getYear(), givenDate.getMonthValue(),
+                                givenDate.getDayOfMonth(), 0, 0, 0);
+                var dateTo = LocalDateTime.of(givenDate.getYear(), givenDate.getMonthValue(),
+                                givenDate.getDayOfMonth(), 23, 59, 59);
+                return new Document("$gte", DATE_TIME_MONGODB_FORMAT.format(dateFrom))
+                                .append("$lte", DATE_TIME_MONGODB_FORMAT.format(dateTo));
+        }
 
 }
