24e0c66a45f4ac8dee10134d10ce0d3e5f737e8a
Mathieu Soysal <Mathieu.Soysal@etu.umontpellier.fr>
2022-02-05 21:19:17 +0100

    📝 Add JavaDoc

--- src/main/java/io/github/mathieusoysal/dao/AbstractDao.java
+++ src/main/java/io/github/mathieusoysal/dao/AbstractDao.java
@@ -8,16 +8,36 @@ import org.bson.types.ObjectId;
 
 abstract class AbstractDao {
 
+    /**
+     * The database name.
+     */
     protected final String CODINGAME_DATABASE;
-    protected MongoDatabase db;
+    /**
+     * The {@link MongoClient} used to access the database.
+     */
     protected MongoClient mongoClient;
+    /**
+     * The {@link MongoDatabase} used to interact with the database.
+     */
+    protected MongoDatabase db;
 
+    /**
+     * Creates a new instance of {@link AbstractDao}.
+     * 
+     * @param mongoClient  the {@link MongoClient} used to access the database.
+     * @param databaseName the name of the database.
+     */
     protected AbstractDao(MongoClient mongoClient, String databaseName) {
         this.mongoClient = mongoClient;
         CODINGAME_DATABASE = databaseName;
         this.db = this.mongoClient.getDatabase(CODINGAME_DATABASE);
     }
 
+    /**
+     * Default generation of the {@link ObjectId} used to identify the document.
+     * 
+     * @return the {@link ObjectId} used to identify the document.
+     */
     public ObjectId generateObjectId() {
         return new ObjectId();
     }
