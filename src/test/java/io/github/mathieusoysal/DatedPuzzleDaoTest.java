de82fe09b610cad74b7399868c1c1e1de0d382d9
Mathieu Soysal <Mathieu.Soysal@etu.umontpellier.fr>
2022-02-05 17:00:11 +0100

    🧪 Add failing test DatedPuzzleDao

--- src/test/java/io/github/mathieusoysal/DatedPuzzleDaoTest.java
+++ src/test/java/io/github/mathieusoysal/DatedPuzzleDaoTest.java
@@ -3,9 +3,8 @@ package io.github.mathieusoysal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.bson.Document;
@@ -19,38 +18,38 @@ import io.github.mathieusoysal.util.MongoDBMock;
 
 class DatedPuzzleDaoTest extends MongoDBMock {
 
-    private DatedPuzzlesDao puzzleDao;
+        private DatedPuzzlesDao puzzleDao;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        super.setUp();
-        puzzleDao = new DatedPuzzlesDao(mongoClient, "CodinGame-stats");
-    }
+        @BeforeEach
+        public void setUp() throws Exception {
+                super.setUp();
+                puzzleDao = new DatedPuzzlesDao(mongoClient, "CodinGame-stats");
+        }
 
-    @AfterEach
-    public void tearDown() throws Exception {
-        mongoClient.getDatabase("CodinGame-stats")
-                .getCollection(DatedPuzzlesDao.PUZZLES_HISTORY_COLLECTION)
-                .deleteMany(new Document());
-        super.tearDown();
-    }
+        @AfterEach
+        public void tearDown() throws Exception {
+                mongoClient.getDatabase("CodinGame-stats")
+                                .getCollection(DatedPuzzlesDao.PUZZLES_HISTORY_COLLECTION)
+                                .deleteMany(new Document());
+                super.tearDown();
+        }
 
-    @Test
-    void testGetPuzzlesOfDate() {
-        mongoClient.getDatabase("CodinGame-stats")
-                .getCollection(DatedPuzzlesDao.PUZZLES_HISTORY_COLLECTION)
-                .insertMany(Arrays.asList(
-                        new Document("date", new Date(new GregorianCalendar(2020 + 1900, 1, 1).getTimeInMillis()))
-                                .append("puzzle", new Document("id", "1")),
-                        new Document("date", new Date(new GregorianCalendar(2020 + 1900, 2, 1).getTimeInMillis()))
-                                .append("puzzle", new Document("id", "2")),
-                        new Document("date", new Date(new GregorianCalendar(2020 + 1900, 3, 3).getTimeInMillis()))
-                                .append("puzzle", new Document("id", "3"))));
+        @Test
+        void testGetPuzzlesOfDate() {
+                mongoClient.getDatabase("CodinGame-stats")
+                                .getCollection(DatedPuzzlesDao.PUZZLES_HISTORY_COLLECTION)
+                                .insertMany(Arrays.asList(
+                                                new Document("date", "2020-01-01T00:00:00.000+00:00")
+                                                                .append("puzzle", new Document("id", "1")),
+                                                new Document("date", "2020-02-01T00:00:00.000+00:00")
+                                                                .append("puzzle", new Document("id", "2")),
+                                                new Document("date", "2020-03-01T00:00:00.000+00:00")
+                                                                .append("puzzle", new Document("id", "3"))));
 
-        List<DatedPuzzle> puzzles = puzzleDao.getPuzzlesOf(new GregorianCalendar(2020 + 1900, 1, 1).getTime());
+                List<DatedPuzzle> puzzles = puzzleDao.getPuzzlesOf(LocalDate.of(2020, 1, 1));
 
-        assertEquals(1, puzzles.size());
-        assertEquals(1, puzzles.get(0).getPuzzle().getId());
-    }
+                assertEquals(1, puzzles.size());
+                assertEquals(1, puzzles.get(0).getPuzzle().getId());
+        }
 
 }
